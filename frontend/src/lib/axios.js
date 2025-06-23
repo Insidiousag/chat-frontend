19858920a25315161ecd180febe7f22ae2ababc4
Insidiousag <aman19022004@gmail.com>
2025-06-24 01:03:08 +0530

    Refactor authentication controller to return JWT token in signup and login responses

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,21 @@
-// src/axios.js
+// // src/axios.js
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+//   withCredentials: true,
+// });
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  baseURL: "https://chat-backend-bbts.onrender.com/api",
+});
+
+// Automatically attach token in all requests
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
